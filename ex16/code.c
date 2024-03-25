7ac61a429438a796d50bb3eebcdf55d0cd099769
Gabriel Matias <gabrielpereiramatias07@gmail.com>
2024-03-25 10:02:22 -0300

    Add

---
+++ ex16/code.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int a, b, s, maiorAB, result;
+    scanf("%d %d %d", &a, &b, &s);
+    maiorAB = (a + b + abs(a - b)) / 2;
+    result = (maiorAB+ s + abs(maiorAB - s)) / 2;
+    printf("%d eh o maior\n", result);
+    return 0;
+}
